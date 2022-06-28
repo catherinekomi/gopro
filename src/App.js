af0ab109ee72a90ea319c37ad4d4125c44ccbe3b
Catherine Komi <oceanbeaats@gmail.com>
2022-06-27 21:59:15 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+
+const App = () => (
+  <>
+    <Navbar />
+    <Header />
+  </>
+);
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Hey How Are you</p>
+      <h5> is it working?</h5>
+    </div>
+  );
+}
+
+export default Navbar;
