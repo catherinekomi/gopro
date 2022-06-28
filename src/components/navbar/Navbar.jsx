f58256641cbc4dab55e2995ec84884ec08aa75a0
Catherine Komi <oceanbeaats@gmail.com>
2022-06-27 22:55:08 -0700

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,14 +5,60 @@ import "./navbar.css";
 import Logo from "../../assets/goprologo.svg";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
-    <>
-      <nav className="navbar">
-        <Link to="/" className="navbar-logo">
-          <img src={Logo} alt="GoPro Logo" />
-        </Link>
-      </nav>
-    </>
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
+        <img src={Logo} alt="GoPro Logo" />
+      </Link>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+      </div>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+          <Link to="./" className="nav-links" onClick={closeMobileMenu}>
+            Cameras
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="./apps" className="nav-links" onClick={closeMobileMenu}>
+            Apps
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="./accessories"
+            className="nav-links"
+            onClick={closeMobileMenu}
+          >
+            Accessories
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="./gear" className="nav-links" onClick={closeMobileMenu}>
+            Lifestyly Gear
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="./subscription"
+            className="nav-links"
+            onClick={closeMobileMenu}
+          >
+            GoPro Subscription
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="./deals" className="nav-links" onClick={closeMobileMenu}>
+            Deals <i className="fas fa-caret-down" />
+          </Link>
+          {dropdown && <Dropdown />}
+        </li>
+      </ul>
+    </nav>
   );
 }
 
